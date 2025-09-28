73a205d5ae4ca15d9daa096e69950166751bc0c3
Silverspine1 <samuelbucke@outlook.com>
2025-09-28 18:32:23 +0200

    low mid high and in zone added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBase/teleOP/First_Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBase/teleOP/First_Tele.java
@@ -10,6 +10,20 @@ public class First_Tele extends OpModeEX {
 
     @Override
     public void loopEX() {
+        driveBase.drivePowers(gamepad1.right_stick_y,(gamepad1.left_trigger - gamepad1.right_trigger),gamepad1.left_stick_x);
+        if (gamepad1.right_bumper){
+            intake.intakeMotor.update(-1);
+
+        }else if (gamepad1.left_bumper){
+            intake.intakeMotor.update(1);
+        }else {
+            intake.intakeMotor.update(0);
+        }
+        if (gamepad1.a){
+            intake.trans.setPosition(1);
+        }
+        telemetry.addData("in zone",turret.inZone);
+
 
     }
 }
